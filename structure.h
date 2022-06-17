95b301d5fbdeed4a6ed8147358ebb609d34288ad
demoven <102261759+demoven@users.noreply.github.com>
2022-06-17 14:20:08 +0200

    Reglage lune
    - lune fonctionnelle
    - fonction save
    - reglage affichage

--- structure.h
+++ structure.h
@@ -2,9 +2,13 @@ typedef struct Planete
 {
     char nom[20];
     float x;
+    float x_absolu;
     float y;
+    float y_absolu;
     float vx;
     float vy;
+    float ax;
+    float ay;
     float force;
     double Distance_orbit;
     double Distance_reelle;
